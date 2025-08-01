e6a59f859fe2097707684e295625551eb438c8ff
Flavio-LP <flavio5070@outlook.com>
2025-08-01 16:47:06 -0300

    ajustando funcionamento do carro

--- src/br/com/car/Car.java
+++ src/br/com/car/Car.java
@@ -66,43 +66,54 @@ public class Car {
         }
     }
 
-
     public void Acelerar(){
 
         if(!VerificaCarro()){
             System.out.println("Velocidade não aplicada!");
         } else{
 
-            if (this.velocidade <= 20 ){
-                if (this.marcha < 2){
+            System.out.println("velocidade: "+ this.velocidade);
+            System.out.println("marcha: " + this.marcha);
+
+            if(this.marcha == 0 ){
+               this.marcha++;
+               this.velocidade++;
+               System.out.println("Marcha trocada!");
+               System.out.println("Velocidade aplicada!");
+               return;
+            }
+
+            if (this.marcha == 1){
+
+                if (this.velocidade < 20 ){
                     this.velocidade++;
                     System.out.println("Velocidade aplicada!");
                 }else{
                     System.out.println("Velocidade máxima atingida na marcha 1!");
                 }
-            } else if (this.velocidade <= 40 ){
-                if (this.marcha < 3){
+            } else if (this.marcha == 2){
+                if (this.velocidade < 40 ){
                     this.velocidade++;
                     System.out.println("Velocidade aplicada!");
                 }else{
                     System.out.println("Velocidade máxima atingida na marcha 2!");
                 }
-            } else if (this.velocidade <= 60 ){
-                if (this.marcha < 4){
+            } else if (this.marcha == 3){
+                if (this.velocidade < 60 ){
                     this.velocidade++;
                     System.out.println("Velocidade aplicada!");
                 }else{
                     System.out.println("Velocidade máxima atingida na marcha 3!");
                 }
-            } else if (this.velocidade <= 80 ){
-                if (this.marcha < 5){
+            } else if (this.marcha == 4){
+                if (this.velocidade < 80){
                     this.velocidade++;
                     System.out.println("Velocidade aplicada!");
                 }else{
                     System.out.println("Velocidade máxima atingida na marcha 4!");
                 }
-            } else if (this.velocidade <= 100 ){
-                if (this.marcha < 6){
+            } else if (this.marcha == 5 ){
+                if (this.velocidade < 100){
                     this.velocidade++;
                     System.out.println("Velocidade aplicada!");
                 }else{
@@ -113,7 +124,7 @@ public class Car {
                     this.velocidade++;
                     System.out.println("Velocidade aplicada!");
                 }else{
-                    System.out.println("Velocidade máxima atingida na marcha 6!");
+                    System.out.println("Velocidade máxima atingida na marcha 5!");
                 }
             }
         }
@@ -122,11 +133,17 @@ public class Car {
     }
 
     public void Desacelerar(){
-        if (this.velocidade > 0) {
-            this.velocidade--;
-            System.out.println("Velocidade aplicada");
-        }else{
-            System.out.println("Velocidade mínima atingida!");
+
+        if(!VerificaCarro()){
+            System.out.println("Velocidade não aplicada!");
+        } else {
+
+            if (this.velocidade > 0) {
+                this.velocidade--;
+                System.out.println("Velocidade aplicada");
+            } else {
+                System.out.println("Velocidade mínima atingida!");
+            }
         }
 
     }
@@ -134,8 +151,8 @@ public class Car {
     public void TrocarMarcha(int opcao){
 
         if (opcao == 1){
-            if (this.marcha < 1){
-                if (this.velocidade > 0 && this.velocidade < 20){
+            if (this.marcha <= 1){
+                if (this.velocidade > 0 && this.velocidade <= 20){
                     this.marcha++;
                     System.out.println("Marcha Trocada!");
                 } else{
@@ -187,12 +204,60 @@ public class Car {
 
 
         } else{
-            if (this.marcha > 0){
-                this.marcha--;
-                System.out.println("Marcha trocada!");
-            }else{
-                System.out.println("Marcha mínima atingida!");
+
+            if (this.marcha == 1 && this.marcha > 0 ){
+                if (this.velocidade > 0 && this.velocidade <= 20){
+                    this.marcha--;
+                    System.out.println("Marcha Trocada!");
+                } else{
+                    System.out.println("Velocidade incorreta");
+                }
+
             }
+            else if (this.marcha < 2){
+                if (this.velocidade > 21 && this.velocidade < 40){
+                    this.marcha++;
+                    System.out.println("Marcha Trocada!");
+                } else{
+                    System.out.println("Velocidade incorreta");
+                }
+
+            } else if (this.marcha < 3){
+                if (this.velocidade >= 41 && this.velocidade < 60){
+                    this.marcha--;
+                    System.out.println("Marcha Trocada!");
+                } else{
+                    System.out.println("Velocidade incorreta");
+                }
+
+            } else if (this.marcha < 4){
+                if (this.velocidade >= 61 && this.velocidade < 80){
+                    this.marcha--;
+                    System.out.println("Marcha Trocada!");
+                } else{
+                    System.out.println("Velocidade incorreta");
+                }
+
+            } else if  (this.marcha < 5){
+                if (this.velocidade >= 81 && this.velocidade < 100){
+                    this.marcha--;
+                    System.out.println("Marcha Trocada!");
+                } else{
+                    System.out.println("Velocidade incorreta");
+                }
+
+            } else{
+                if (this.velocidade >= 101 && this.velocidade < 120){
+                    this.marcha++;
+                    System.out.println("Marcha Trocada!");
+                } else{
+                    System.out.println("Marcha mínima atingida!");
+                }
+            }
+
+
+
+
         }
 
     }
